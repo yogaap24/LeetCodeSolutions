730505c9cda292f54c82c1b6446e34ce9b29cb4b
yogaap24 <yogaagungprasetio24@gmail.com>
2024-12-09 10:54:05 +0700

    Time: 1 ms (93.85%), Space: 8.9 MB (39.71%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.go
@@ -0,0 +1,42 @@
+func maximalSquare(matrix [][]byte) int {
+    m := len(matrix)
+    n := len(matrix[0])
+
+    dp := make([][]int, m)
+    for i := range dp {
+        dp[i] = make([]int, n)
+    }
+
+    maxSide := 0;
+
+    for i := 0; i < m; i++ {
+        for j := 0; j < n; j++ {
+            if matrix[i][j] == '1' {
+                if i == 0 || j == 0 {
+                    dp[i][j] = 1
+                } else {
+                    dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
+                }
+
+                if dp[i][j] > maxSide {
+                    maxSide = dp[i][j]
+                }
+            }
+        }
+    }
+
+    return maxSide * maxSide
+}
+
+func min(a, b, c int) int {
+    if a < b {
+        if a < c {
+            return a
+        }
+        return c
+    }
+    if b < c {
+        return b
+    }
+    return c
+}
